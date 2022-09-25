da95915c0ba2e53c533763d84b9b9e916e7f1ff1
ADMIN\Admin <thanhpdhe141032@gmail.com>
2022-09-25 19:51:40 +0700

    update authen exception

--- src/main/java/com/example/backendbase/common/exceptions/ExceptionHandlers.java
+++ src/main/java/com/example/backendbase/common/exceptions/ExceptionHandlers.java
@@ -5,6 +5,7 @@ import com.example.backendbase.common.entity.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,6 +29,17 @@ public class ExceptionHandlers {
     @ExceptionHandler(UsernameNotFoundException.class)
     @org.springframework.web.bind.annotation.ResponseStatus(HttpStatus.FORBIDDEN)
     public Response handleNotFoundUsername(UsernameNotFoundException e) {
+        var responseStatus = ResponseStatus.builder()
+                .code(HttpStatus.FORBIDDEN.value())
+                .message(e.getMessage())
+                .build();
+        e.printStackTrace();
+        return new Response(responseStatus, null);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @org.springframework.web.bind.annotation.ResponseStatus(HttpStatus.FORBIDDEN)
+    public Response handleAuthException(AccessDeniedException e) {
         var responseStatus = ResponseStatus.builder()
                 .code(HttpStatus.FORBIDDEN.value())
                 .message(e.getMessage())
--- src/main/java/com/example/backendbase/security/configurations/AuthTokenFilter.java
+++ src/main/java/com/example/backendbase/security/configurations/AuthTokenFilter.java
@@ -16,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 
 @Slf4j
 public class AuthTokenFilter extends OncePerRequestFilter {
@@ -29,24 +30,20 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        try {
-            String jwt = parseJwt(request);
-            if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
-                String username = jwtUtils.getUserNameFromJwtToken(jwt);
+        String jwt = parseJwt(request);
+        if (jwt == null && !jwtUtils.validateJwtToken(jwt)) {
+            String username = jwtUtils.getUserNameFromJwtToken(jwt);
 
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-                UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(userDetails,
-                                null,
-                                userDetails.getAuthorities());
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails,
+                            null,
+                            userDetails.getAuthorities());
 
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        } catch (Exception e) {
-            logger.error("Cannot set user authentication: {}", e);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         }
 
         filterChain.doFilter(request, response);
