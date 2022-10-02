18bb37153ba12de11574a7997f3b5522bcfff92d
hieucol123 <hieutmse04225@fpt.edu.vn>
2022-10-03 00:41:21 +0700

    fix confict

--- src/main/java/com/example/backendbase/manager/service/BuildingManagerServiceImpl.java
+++ src/main/java/com/example/backendbase/manager/service/BuildingManagerServiceImpl.java
@@ -4,7 +4,6 @@ import com.example.backendbase.common.utils.TimeUtils;
 import com.example.backendbase.manager.entity.Address;
 import com.example.backendbase.manager.entity.Buildings;
 import com.example.backendbase.manager.entity.request.AddBuildingRequest;
-import com.example.backendbase.manager.entity.request.UpdateBuildingRequest;
 import com.example.backendbase.manager.repo.BuildingRepo;
 import com.example.backendbase.user.util.CurrentUserUtils;
 import lombok.RequiredArgsConstructor;
@@ -24,45 +23,17 @@ public class BuildingManagerServiceImpl implements IBuildingManager{
                 .buildingName(request.getBuildingName())
                 .totalRooms(request.getTotalRoom())
                 .address(Address.builder().city(request.getCity())
-                        .district(request.getDistrict())
-                        .wards(request.getWards())
-                        .moreDetails(request.getMoreAddressDetail())
-                        .createdBy(CurrentUserUtils.getCurrentUser())
-                        .updatedTime(TimeUtils.getCurrentTime())
-                        .build())
+                                .district(request.getDistrict())
+                                .wards(request.getWards())
+                                .moreDetails(request.getMoreAddressDetail())
+                                .createdBy(CurrentUserUtils.getCurrentUser())
+                                .updatedTime(TimeUtils.getCurrentTime())
+                                .build())
                 .createdBy(CurrentUserUtils.getCurrentUser())
                 .updatedTime(TimeUtils.getCurrentTime())
                 .build());
     }
 
-
-    public String updateBuilding(UpdateBuildingRequest request) {
-        Buildings building = buildingRepo.findById(request.getBuildingId()).orElseThrow(() -> new RuntimeException("Error: Buildings is not found."));
-        building.setBuildingName(request.getBuildingName());
-        building.setTotalRooms(request.getTotalRoom());
-
-        //set address
-        building.getAddress().setCity(request.getCity());
-        building.getAddress().setDistrict(request.getDistrict());
-        building.getAddress().setWards(request.getWards());
-        building.getAddress().setMoreDetails(request.getMoreAddressDetail());
-        building.getAddress().setCreatedBy(CurrentUserUtils.getCurrentUser());
-        building.getAddress().setUpdatedTime(TimeUtils.getCurrentTime());
-
-
-        building.setCreatedBy(CurrentUserUtils.getCurrentUser());
-        building.setUpdatedTime(TimeUtils.getCurrentTime());
-
-        //update
-        buildingRepo.save(building);
-        return "update successfully";
-    }
-
-    @Override
-    public Buildings getBuilding(Long buildingId) {
-        return buildingRepo.findById(buildingId).orElseThrow(() -> new RuntimeException("Error: Buildings is not found."));
-    }
-    
     @Override
     public List<Buildings> getAllBuilding() {
         return buildingRepo.getAllBuilding();
